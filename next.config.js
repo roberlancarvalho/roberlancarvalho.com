8a84a58aa528e2403158f56bb2b2a977f4d5b6d1
Roberlan Carvalho <roberlan.carvalho@gmail.com>
2025-03-13 12:56:18 -0300

    ativando PWA

--- next.config.js
+++ next.config.js
@@ -1,74 +1,81 @@
-const withPWA = require('next-pwa')({
-  dest: 'public', // define o destino dos arquivos do service worker
-  disable: process.env.NODE_ENV === 'development' // desativa PWA em desenvolvimento
-})
+const withPWA = require("next-pwa")({
+  dest: "public", // Define a pasta onde os arquivos do PWA serão gerados
+  disable: process.env.NODE_ENV === "development", // Desativa PWA no modo de desenvolvimento
+});
 
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true'
-})
-module.exports = withBundleAnalyzer({})
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true", // Ativa a análise do bundle quando a variável ANALYZE=true
+});
 
-module.exports = withPWA({
-  webpack: (config, { dev, isServer }) => {
-    if (!dev && !isServer) {
-      Object.assign(config.resolve.alias, {
-        react: 'preact/compat',
-        'react-dom/test-utils': 'preact/test-utils',
-        'react-dom': 'preact/compat'
-      })
-    }
+module.exports = withBundleAnalyzer(
+  withPWA({
+    reactStrictMode: true, // Ativa o modo estrito do React para detectar possíveis problemas na aplicação
 
-    return config
-  },
-  async rewrites() {
-    return [
-      { source: '/js/', destination: 'https://roberlancarvalho.com/' },
-      { source: '/jekyll/', destination: 'https://roberlancarvalho.com/' },
-      { source: '/svg/', destination: 'https://roberlancarvalho.com/' },
-      { source: '/dev/', destination: 'https://roberlancarvalho.com/' },
-      { source: '/tags/', destination: 'https://roberlancarvalho.com/' },
-      {
-        source: '/making-of-blog-novo/',
-        destination:
-          'https://roberlancarvalho.com/making-of-blog-novo-gatsby-js'
-      },
-      { source: '/page/:slug*', destination: 'https://roberlancarvalho.com/' }
-    ]
-  },
-  // async redirects() {
-  //   return [
-  //     {
-  //       source: '/my-trips/',
-  //       destination: 'https://my-trips.willianjusten.com.br/',
-  //       permanent: true
-  //     },
-  //     {
-  //       source: '/japanese-words/',
-  //       destination: 'https://japanese-words.willianjusten.com.br/',
-  //       permanent: true
-  //     },
-  //     {
-  //       source: '/learn-japanese/',
-  //       destination: 'https://learn-japanese.willianjusten.com.br/',
-  //       permanent: true
-  //     },
-  //     {
-  //       source: '/js-com-tdd-na-pratica-na-udemy/',
-  //       destination: 'https://www.youtube.com/@WillianJustenCursos',
-  //       permanent: true
-  //     },
-  //     {
-  //       source: '/novo-curso-aprenda-criar-sites-animados-greensock/',
-  //       destination: 'https://willianjusten.com.br',
-  //       permanent: true
-  //     },
-  //     {
-  //       source:
-  //         '/novo-curso-gatsby-crie-um-site-pwa-com-react-graphql-netlify-cms/',
-  //       destination: 'https://willianjusten.com.br',
-  //       permanent: true
-  //     }
-  //   ]
-  // },
-  images: { domains: ['pbs.twimg.com', 'avatars.githubusercontent.com'] }
-})
+    webpack: (config, { dev, isServer }) => {
+      // Usa Preact para otimizar o bundle no ambiente de produção
+      if (!dev && !isServer) {
+        Object.assign(config.resolve.alias, {
+          react: "preact/compat",
+          "react-dom/test-utils": "preact/test-utils",
+          "react-dom": "preact/compat",
+        });
+      }
+      return config;
+    },
+
+    async rewrites() {
+      return [
+        // Redirecionamentos personalizados
+        { source: "/js/", destination: "https://roberlancarvalho.com/" },
+        { source: "/jekyll/", destination: "https://roberlancarvalho.com/" },
+        { source: "/svg/", destination: "https://roberlancarvalho.com/" },
+        { source: "/dev/", destination: "https://roberlancarvalho.com/" },
+        { source: "/tags/", destination: "https://roberlancarvalho.com/" },
+        {
+          source: "/making-of-blog-novo/",
+          destination: "https://roberlancarvalho.com/making-of-blog-novo-gatsby-js",
+        },
+        { source: "/page/:slug*", destination: "https://roberlancarvalho.com/" },
+      ];
+    },
+
+    // async redirects() {
+    //   return [
+    //     {
+    //       source: "/my-trips/",
+    //       destination: "https://my-trips.willianjusten.com.br/",
+    //       permanent: true,
+    //     },
+    //     {
+    //       source: "/japanese-words/",
+    //       destination: "https://japanese-words.willianjusten.com.br/",
+    //       permanent: true,
+    //     },
+    //     {
+    //       source: "/learn-japanese/",
+    //       destination: "https://learn-japanese.willianjusten.com.br/",
+    //       permanent: true,
+    //     },
+    //     {
+    //       source: "/js-com-tdd-na-pratica-na-udemy/",
+    //       destination: "https://www.youtube.com/@WillianJustenCursos",
+    //       permanent: true,
+    //     },
+    //     {
+    //       source: "/novo-curso-aprenda-criar-sites-animados-greensock/",
+    //       destination: "https://willianjusten.com.br",
+    //       permanent: true,
+    //     },
+    //     {
+    //       source: "/novo-curso-gatsby-crie-um-site-pwa-com-react-graphql-netlify-cms/",
+    //       destination: "https://willianjusten.com.br",
+    //       permanent: true,
+    //     },
+    //   ];
+    // },
+
+    images: {
+      domains: ["pbs.twimg.com", "avatars.githubusercontent.com"], // Permite imagens externas desses domínios
+    },
+  })
+);
